77e25b4806f068ba2a174ecf44331e4bbbdcd6d8
M Aakif Raza <simply.aakif@gmail.com>
2018-09-28 16:05:41 +0500

    Plugin Start

---
+++ ar_collaborative_reports.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  AR Collaborative Reports
+Plugin URI:   https://ace.org.pk/
+Description:  Wordpress reporting system for CreditVision
+Version:      0.27
+Author:       Muhammad Aakif Raza
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
